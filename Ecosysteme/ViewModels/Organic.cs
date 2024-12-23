3085fb07dbbbe495279abf9bf8cc9ea68d6bc47f
21242NS <Nicolasschell234@gmail.com>
2024-12-23 11:56:46 +0100

    bug

--- Ecosysteme/ViewModels/Organic.cs
+++ Ecosysteme/ViewModels/Organic.cs
@@ -4,12 +4,14 @@ using CommunityToolkit.Mvvm.ComponentModel;
 
 namespace Pong.ViewModels;
 
-public partial class Organic : GameObject {
+public partial class Organic : Not_a_life {
     [ObservableProperty]
     private int fresh;
     public Organic(Point location):base(location) {
         this.Fresh=100;
     }
+    [ObservableProperty]
+    private bool erased =false;
     public ObservableCollection<GameObject> Tick(ObservableCollection<GameObject> gameobjects){
         return gameobjects;
     }
