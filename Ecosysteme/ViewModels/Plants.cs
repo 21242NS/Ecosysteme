b86f2b1ec068e8d0c4cffc937ac909cbadcab06e
21242NS <Nicolasschell234@gmail.com>
2024-12-23 18:43:07 +0100

    interface

--- Ecosysteme/ViewModels/Plants.cs
+++ Ecosysteme/ViewModels/Plants.cs
@@ -6,7 +6,7 @@ using CommunityToolkit.Mvvm.ComponentModel;
 
 namespace Pong.ViewModels;
 
-public abstract partial class Plants : Form_of_life {
+public abstract partial class Plants : Form_of_life, Range {
     [ObservableProperty]
     private float range_of_food;
     [ObservableProperty]
